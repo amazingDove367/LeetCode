c3e0f6faf37630fee454a2eda2f48dff880b7e93
Hakyung Sohn <hortenssiaa@gmail.com>
2025-09-01 12:38:54 +0900

    Time: 0 ms (100%), Space: 19.4 MB (94.85%) - LeetHub

---
+++ 0136-single-number/0136-single-number.swift
@@ -0,0 +1,11 @@
+// ^ : 비트 연산자 XOR 
+
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        var result = 0
+        for num in nums {
+            result ^= num
+        }
+        return result
+    }
+}
