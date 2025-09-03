1dee3a9e49412c1bea7c1e51c6d25ad442f19ade
Hakyung Sohn <hortenssiaa@gmail.com>
2025-09-03 16:16:32 +0900

    Time: 0 ms (100%), Space: 19.6 MB (76.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,26 +1,44 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        if nums.count == 2 { return [0, 1] }
+        var indexByValue = [Int: Int]()
+        indexByValue.reserveCapacity(nums.count)
 
-        var hashmap = [Int: [Int]]()
         for (idx, val) in nums.enumerated() {
-            hashmap[val, default: []].append(idx) // ⭐️
+            var need = target - val
+            if let v = indexByValue[need] { 
+                // nums: [3, 2, 4] 가능한 이유는
+                // idx 0 일때, 아직 indexByValue 에 아무것도 없음 따라서, 지나감
+                return [idx, v]
+            } 
+            indexByValue[val] = idx
         }
-
-        if target % 2 == 0 {
-            if let arr = hashmap[target / 2], arr.count == 2 {
-                return hashmap[target / 2]!
-            }
-        }
-
-        for (val, idxArr) in hashmap {
-            var sub = target - val
-            if let indices = hashmap[sub] {
-                if sub == val { continue }
-                return [idxArr[0], indices[0]]
-            }
-        } 
-
         return []
     }
 }
+
+// class Solution {
+//     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+//         if nums.count == 2 { return [0, 1] }
+
+//         var hashmap = [Int: [Int]]()
+//         for (idx, val) in nums.enumerated() {
+//             hashmap[val, default: []].append(idx) // ⭐️
+//         }
+
+//         if target % 2 == 0 {
+//             if let arr = hashmap[target / 2], arr.count == 2 {
+//                 return hashmap[target / 2]!
+//             }
+//         }
+
+//         for (val, idxArr) in hashmap {
+//             var sub = target - val
+//             if let indices = hashmap[sub] {
+//                 if sub == val { continue }
+//                 return [idxArr[0], indices[0]]
+//             }
+//         } 
+
+//         return []
+//     }
+// }
